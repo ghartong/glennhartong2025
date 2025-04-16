8cf0e71eff25ff8a88f8602a745b421b42d479dc
ghartong <glenn@glickcf.com>
2025-04-16 02:05:35 -0400

    bug fixes

--- app/repairshop/customers/form/CustomerForm.tsx
+++ app/repairshop/customers/form/CustomerForm.tsx
@@ -68,7 +68,7 @@ export default function CustomerForm({ customer, isManager = false }: Props) {
 
     useEffect(() => {
         form.reset(hasCustomerId ? defaultValues : emptyValues)
-    }, [searchParams.get("customerId")])
+    }, [searchParams.get("customerId")]) // eslint-disable-line react-hooks/exhaustive-deps
 
     const {
         execute: executeSave,
