c6dd43af69d9d84c354a97fefe1294937b053148
ghartong <glenn@glickcf.com>
2025-04-16 15:34:36 -0400

    portfolio additions and favicon

--- app/(resume)/portfolio/utils.tsx
+++ app/(resume)/portfolio/utils.tsx
@@ -3,8 +3,8 @@ export const sites = [
         id: 1,
         name: 'Glenn Hartong dot com',
         demoLink: '',
-        externalLink: '',
-        externalLinkText: '',
+        externalLink: 'https://github.com/ghartong/glennhartong2025',
+        externalLinkText: 'View on GitHub',
         shortDesc: 'Portfolio and Personal Website',
         longDesc: 'Designed and developed a personal portfolio site using React and Tailwind CSS to showcase projects and stay current with modern frontend technologies. The site serves as a sandbox for prototyping new ideas, with a strong emphasis on clean UI, intuitive UX, and accessibility best practices.',
         tech: [
@@ -22,11 +22,11 @@ export const sites = [
     {
         id: 2,
         name: 'Computer Repair Shop',
-        demoLink: '/repairshop',
+        demoLink: '',
         externalLink: 'https://youtube.com/playlist?list=PL0Zuz27SZ-6PCLz7VMP2QQdeKa83rshe5&feature=shared',
         externalLinkText: 'View on Youtube',
         shortDesc: 'Tutorial by Dave Gray',
-        longDesc: `Followed Dave Gray’s “Next.js Full Stack Project” tutorial to build a full-stack application using Next.js 15, React 19, and Drizzle ORM. This project served as a hands-on introduction to Drizzle and modern Next.js features. Includes a live demo and full tutorial series available on YouTube for deeper insight into the development process.`,
+        longDesc: `Followed Dave Gray's “Next.js Full Stack Project” tutorial to build a full-stack application using Next.js 15, React 19, and Drizzle ORM. This project served as a hands-on introduction to Drizzle and modern Next.js features. Includes a live demo and full tutorial series available on YouTube for deeper insight into the development process.`,
         tech: [
             'NextJS 15 & React 19',
             'shadcn (UI)',
@@ -41,19 +41,131 @@ export const sites = [
     },
     {
         id: 3,
+        name: 'React-a-Mole',
+        demoLink: '',
+        externalLink: 'https://github.com/ghartong/react-a-mole',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'Whack-a-Mole in React',
+        longDesc: '2019 - A job interview asked for a vanilla JS version of whack-a-mole. I had so much fun writing it that I wanted to do it in React too. This is older code, but it should still run. No extra frameworks or libraries aside from React and Redux.',
+        tech: [
+            'React',
+            'Redux',
+        ]
+    },
+    {
+        id: 4,
+        name: 'Whack-a-Mole (the original)',
+        demoLink: '',
+        externalLink: 'https://github.com/ghartong/react-a-mole',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'Vanilla JS Whack-a-Mole game',
+        longDesc: '2019 - This is the original version of my React-a-Mole. No extra frameworks or libraries, just HTML, CSS, and JS. The point was to build it completely in vanilla JS for a job interview. As such, the code is very verbose.',
+        tech: [
+            'HTML',
+            'CSS',
+            'vanilla JS',
+        ]
+    },
+    {
+        id: 5,
+        name: 'Tufted Puffins Electron App',
+        demoLink: '',
+        externalLink: 'https://github.com/ghartong/tufted-puffins',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'Local Band App for Playing Demos',
+        longDesc: '2023 - A friend was a drummer in a local band and also a fellow developer. He asked me to help him with a started app that could play their demos. This is built with Electron and React.',
+        tech: [
+            'React',
+            'Electron',
+        ]
+    },
+    {
+        id: 6,
+        name: 'React wargames prototype',
+        demoLink: '',
+        externalLink: 'https://github.com/ghartong/wargames',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'Lords of Mobile clone built in React',
+        longDesc: '2023 - Concept application of a popular Android game re-imagined in React. I did not add graphics or complete this project. It was just a fun distraction.',
+        tech: [
+            'React',
+            'Module CSS',
+        ]
+    },
+    {
+        id: 7,
         name: 'Victory Lodge Membership App',
         demoLink: '',
-        externalLink: '',
-        externalLinkText: '',
-        shortDesc: 'This app is no longer available',
-        longDesc: '2017 - An Android application that allows a Masonic Lodge to scan memebership card barcodes for authentication against the Grand Lodge of Ohio membership database. Built with Electron and React.',
+        externalLink: 'https://github.com/ghartong/electronBarcodeReader',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'This app is no longer available on Google Play',
+        longDesc: '2019 - An Android application that allows a Masonic Lodge to scan memebership card barcodes for authentication against the Grand Lodge of Ohio membership database. Built with Electron and React.',
         tech: [
             'React/Redux',
             'Electron',
         ]
     },
     {
-        id: 4,
+        id: 8,
+        name: 'React Native - Push Notify',
+        demoLink: '',
+        externalLink: 'https://github.com/ghartong/RN-pushNotify',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'React Native Demo',
+        longDesc: '2019 - I was learning about React Native. This is a demo for push notifications.',
+        tech: [
+            'React',
+            'React Native',
+            'Redux',
+            'Jest',
+        ]
+    },
+    {
+        id: 9,
+        name: 'React Native - Weather App',
+        demoLink: '',
+        externalLink: 'https://github.com/ghartong/RN-weather-app',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'React Native Demo',
+        longDesc: '2019 - I was learning about React Native. This is a demo weather app. It uses fetch to get external data and permissions to get location from the phone.',
+        tech: [
+            'React',
+            'React Native',
+            'Jest',
+        ]
+    },
+    {
+        id: 10,
+        name: 'Publishing Site',
+        demoLink: '',
+        externalLink: 'https://github.com/ghartong/react-falcor',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'React demo',
+        longDesc: '2017 - Fun publishing site demo to try out Mongo and Falcor.',
+        tech: [
+            'Node',
+            'Express',
+            'React/Redux',
+            'Mongo/Mongoose',
+            'Falcor'
+        ]
+    },
+    {
+        id: 11,
+        name: 'Auction Site',
+        demoLink: '',
+        externalLink: 'https://github.com/ghartong/auction',
+        externalLinkText: 'View on GitHub',
+        shortDesc: 'React based auction site',
+        longDesc: '2017 - I built an auction site at work in ColdFusion. I decided to try to refactor into JS. This was my first time using Grunt I believe.',
+        tech: [
+            'React/Redux',
+            'SASS',
+            'Grunt'
+        ]
+    },
+    {
+        id: 12,
         name: 'Glick Productions',
         demoLink: '',
         externalLink: '',
@@ -68,7 +180,7 @@ export const sites = [
         ]
     },
     {
-        id: 5,
+        id: 13,
         name: 'Flower Garden Playhouse',
         demoLink: '',
         externalLink: '',
@@ -81,7 +193,7 @@ export const sites = [
         ]
     },
     {
-        id: 6,
+        id: 14,
         name: 'Ranch City',
         demoLink: '',
         externalLink: '',
