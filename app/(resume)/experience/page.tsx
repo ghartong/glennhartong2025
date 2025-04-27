87363cdf212ba6093a129428181613acb80ea031
ghartong <glenn@glickcf.com>
2025-04-27 14:34:04 -0400

    added Stark Instructor

--- app/(resume)/experience/page.tsx
+++ app/(resume)/experience/page.tsx
@@ -21,6 +21,11 @@ async function Experience() {
     return (
         <>
             <h1 className="font-extrabold">Experience</h1>
+            <section className="w-full lg:w-1/2">
+                Glenn Hartong is an experienced software engineer with a focus on front-end development in ReactJS. Most recently serving as a Senior Front End Engineer II at RVshare from August 2019 to April 2025, 
+                Glenn has previously held roles as UI Developer III at Signet Jewelers and UI Developer II at Sterling Jewelers. A background in programming and website maintenance is further 
+                supported by a Senior Engineer position at WRIS Web Services. Additionally, Glenn co-founded Glick Productions and contributed to education as an Adjunct Instructor at Stark State College of Technology, teaching web development classes.
+            </section>
             <section className="w-full mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {results.map(r => (
                     <ListCard
